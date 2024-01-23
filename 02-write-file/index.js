b4213ea81801d5668714832d553fb4980927e162
Andrewz256 <punkz666@gmail.com>
2024-01-24 00:37:12 +0300

    done

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,11 +1,34 @@
 const fs = require('fs');
 const path = require('path');
-let path1 = path.resolve(__dirname,"text.txt");
-function read(path, code) {
-    const readableStream = fs.createReadStream(path, code);
-    readableStream.on('data', (stream) => {
-        console.log(stream);
-    })
-}
+const readline = require('readline');
 
-read(path1, "utf8");
+const path1 = path.resolve(__dirname, 'text.txt');
+const output1 = fs.createWriteStream(path1);
+
+const readline1 = readline.createInterface(process.stdin, process.stdout);
+
+readline1.question(
+  'Введи текст ( Ctrl+c или exit для выхода) !!!: \n',
+  (string) => {
+    if (string.trim() === 'exit') {
+      readline1.close();
+    } else {
+      output1.write(`${string}\n`);
+      readline1.setPrompt('Введи еще текст!!!: \n');
+      readline1.prompt();
+      readline1.on('line', (string) => {
+        if (string.trim() === 'exit') {
+          readline1.close();
+        } else {
+          readline1.setPrompt('Введи еще текст!!!: \n');
+          output1.write(`${string}\n`);
+          readline1.prompt();
+        }
+      });
+    }
+  },
+);
+
+process.on('exit', () => {
+  console.log('\nПока, _username_, приходите еще');
+});
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
 const path = require('path');
-const path1 = path.resolve(__dirname,"secret-folder");
+const path1 = path.resolve(__dirname, 'secret-folder');
 
-fs.readdir(path1, {withFileTypes: true}, (err,files) => {
-    if (err) throw err;
-    files.forEach(file => {
-        if (file.isDirectory() != true) {
-
-            const fileSize = fs.stat(path.resolve(path1, file.name), (err, stats) => {
-                if (err) return console.log(err);
-                console.log(file.name.split('.').slice(0,1) + " " + file.name.split('.').pop() + " " + stats.size/1024 + "kb");
-            })        
-        }
-    });
+fs.readdir(path1, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+  files.forEach((file) => {
+    if (file.isDirectory() != true) {
+      const fileSize = fs.stat(path.resolve(path1, file.name), (err, stats) => {
+        if (err) return console.log(err);
+        console.log(
+          file.name.split('.').slice(0, 1) +
+            ' ' +
+            file.name.split('.').pop() +
+            ' ' +
+            stats.size / 1024 +
+            'kb',
+        );
+      });
+    }
+  });
 });
